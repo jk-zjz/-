db5716399e30f9635ae4e55366a6207ab3e60141
jk-zjz <3514805258@qq.com>
2022-07-24 14:39:37 +0800

    测试

---
+++ 平时/源.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main() {
+	int age = 0;
+	if (age < 5) {
+		printf("%d", age);
+
+	}
+	else if(age>10)
+	{
+		printf("sdsd");
+	};
+
+	return 0;
+};
